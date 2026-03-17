28516dd462092ea0b7592d433c74b965eb2aadac
tiyegordon <tiye.gordon@gmail.com>
2026-03-17 17:24:55 -0400

    updates

--- part2/config.py
+++ part2/config.py
@@ -1,16 +1,25 @@
 import os
 
-##this is for application settings##
+#this is for application settings
 
 class Config:
-    
-    SECRET_KEY = os.getenv ('Secret_Keu', 'dev-secret-key')
+
+#SECRET KEY
+#Flask uses this key behind the scenes for security purposes and to prevent a crash.
+
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
     DEBUG = False
 
+
 class DevelopmentConfig (Config):
-    Debug = True
+
+#Shows detailed error message,auto restarts server when files are saved, turns app off when not in use
+
+    DEBUG = True
+
+#Available configurations for switching between test, dev, and prod using one word command.
 
 config = {
-        'development': DevelopmentConfig,
-        'default': DeveopmentConfig
-
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+ }
