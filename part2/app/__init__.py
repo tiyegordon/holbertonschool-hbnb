35cf711a6a7d10b5498fe52787fc49be25f041cb
tiyegordon <tiye.gordon@gmail.com>
2026-03-19 11:19:07 -0400

    set up api

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -4,11 +4,12 @@ from config import config #config settings (debug mode and secret key) from conf
 
 def create_app(config_name='default'): 
 
+
     app = Flask(__name__)
 
     app.config.from_object(config[config_name])
 
-    api = Api(app, version ='1.0', title = 'HBnB API', description='HBnB RESful API')
+    api = Api(app, version ='1.0', title = 'HBnB API', description='HBnB RESTful API')
 
     from app.api.v1.user import api as users_ns
     from app.api.v1.amenity import api as amenities_ns
