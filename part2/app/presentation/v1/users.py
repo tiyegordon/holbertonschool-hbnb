62a7c3ff099e0bdc55c0e0cd5db2c4fe3f19aa64
tiyegordon <tiye.gordon@gmail.com>
2026-03-19 10:22:37 -0400

    api setup

--- part2/app/presentation/v1/users.py
+++ part2/app/presentation/v1/users.py
@@ -1,11 +1,17 @@
-from flask_restx import Namespace, Resource, fields
-from app.blayer.facade import Facade
+from flask_restx importNamespace, Resource, fields # Imports the tools to build/define API endpoints
+from app.blayer.facade import Facade #Import facade that connects API to business logic layer (blayer)
 
-api = Namespace('users', description = 'User operations')
+api = Namespace('users', description = 'User operations') #Creates a namespace grouping all user related enpoitns together under one path00
 
-facade = Facade()
+facade = Facade() #Create a shared facade instance so all endpoints in this file can talk to business logic layer
 
-user_model = api.model ('Iser", {
-                        'first_name': fields.String(
-                        required=True,
-                        description='First name of the user'
+#Converts user profile in JSON for API
+user_model = api.model ('User', 
+    {
+    'first_name': fields.String(required=True, description='First name of the user'), # First name is mandatory, can't be left blank
+    'last_name': fields.String(required=True, description='Last name of the user'),# Last name is mandatory, cant be left blank
+    'email': fields.String(required=True, description='Email address of the user'), # Email is mandatory, can't be blank
+    })
+
+
+
