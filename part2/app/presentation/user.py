0faf4aa62ce0536b5a5f97ece9ebd0045d6728d4
tiyegordon <tiye.gordon@gmail.com>
2026-03-25 20:54:06 -0400

    namespace/API structure

--- part2/app/presentation/user.py
+++ part2/app/presentation/user.py
@@ -1,3 +1,32 @@
+#Import/Setup
+
+from flask_restx import Namespace, Resource, fileds
+from app.services.facade import Facade
+
+api = Ma,espace ('user', description = ' User operations')
+facade = Facade()
+
+# Model Definition  = mandotory inputs - first name, last name, and email
+user_model = api.model ('User',
+                       {
+                           'first_name': fields.String (required=True),
+                           'last_name': fields.String(required=True),
+                           'email': fields.String(required=True),
+                           })
+#Endpoints directed to resource class so any thing that htis the path goes to this class
+
+@api.route('/')
+class UserList(Resource):
+    def get(self):          # When a GET request comes in, return all users
+    def post(self):        # When a POST request comes in, create a new user
+
+@api.route('/<id>')
+class UserResource(Resource):
+    def get(self):          # When a GET request comes in, return the one specific user
+    def put(self):          # When a PUT request comes in udpate the one specific user
+
+#API Structure
+
 - namespace: 'user'
 - model fields: first_name, last_name, email
 - endpoints:
