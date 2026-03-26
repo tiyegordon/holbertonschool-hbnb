5d5e1177ef95d7b93f00c6314fa6bc4a603d3f67
tiyegordon <tiye.gordon@gmail.com>
2026-03-25 20:22:26 -0400

    updated flask

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -5,16 +5,18 @@ from config import config #config settings (debug mode and secret key) from conf
 def create_app(config_name='default'): 
 
 
-    app = Flask(__name__)
+    app = Flask(__name__) #creats flask app
 
-    app.config.from_object(config[config_name])
+    app.config.from_object(config[config_name]) #loads settings into 
 
     api = Api(app, version ='1.0', title = 'HBnB API', description='HBnB RESTful API')
 
-    from app.api.v1.user import api as users_ns
-    from app.api.v1.amenity import api as amenities_ns
-    from app.api.v1.place import api as places_ns
-    from app.api.v1.review import api as reviews_ns
+    from app.api.v1.user import api as users_ns  #loads user endpoint
+    from app.api.v1.amenity import api as amenities_ns #loads amentiy endpoints
+    from app.api.v1.place import api as places_ns #loads place endpoints
+    from app.api.v1.review import api as reviews_ns #loads review endpoints
+
+#URLs for each group of endpoints/where endpoints live and communicates location to flask
 
     api.add_namespace(users_ns, path='api/v1/user')
     api.add_namespace(amenities_ns, path='api/v1/amenitiy')
